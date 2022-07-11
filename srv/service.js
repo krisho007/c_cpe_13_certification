09626e9cc3099e5378e1536e9ab9c4a7a300ad3d
Shankaranarayan K <kkammaje@gmail.com>
2022-07-11 04:36:59 +0000

    api key

--- srv/service.js
+++ srv/service.js
@@ -1,6 +1,7 @@
 const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function () {
+    const po = await cds.connect.to('API_PURCHASEORDER_PROCESS_SRV');
 
     this.on('resolve', async (req) => {
         const tx = cds.tx(req);
@@ -10,5 +11,9 @@ module.exports = cds.service.impl(async function () {
                 .where({ ID: req.params[0] })
         );
     });
+
+    this.on('READ', 'PurchaseOrders', async (req) => {
+        return po.run(req.query);
+    });    
 
 });
